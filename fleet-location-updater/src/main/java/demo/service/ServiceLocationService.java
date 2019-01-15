f5f9cb8d60afb3aba03ed67c37967cf95b1a8a82
zhou zhou <zhoujunlin19861115@gmail.com>
2019-01-15 14:36:17 -0500

    Add files via upload

---
+++ fleet-location-updater/src/main/java/demo/service/ServiceLocationService.java
@@ -0,0 +1,15 @@
+
+package demo.service;
+
+import demo.model.CurrentPosition;
+
+/**
+ *
+ *
+ *
+ */
+public interface ServiceLocationService {
+
+	void updateServiceLocations(CurrentPosition currentPosition);
+
+}
