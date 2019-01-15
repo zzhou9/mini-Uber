c03e8055502e58e2437c0533e7818a452a244400
zhou zhou <zhoujunlin19861115@gmail.com>
2019-01-15 14:33:45 -0500

    Add files via upload

---
+++ fleet-location-ingest/src/main/java/demo/FleetLocationIngestApplication.java
@@ -0,0 +1,16 @@
+/*
+ *
+ */
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetLocationIngestApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FleetLocationIngestApplication.class);
+    }
+
+}
