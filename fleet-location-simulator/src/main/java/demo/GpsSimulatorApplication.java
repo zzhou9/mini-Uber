f76603c392f152ffd44b6df229c3bd47c9e0cff5
zhou zhou <zhoujunlin19861115@gmail.com>
2019-01-15 14:35:17 -0500

    Add files via upload

---
+++ fleet-location-simulator/src/main/java/demo/GpsSimulatorApplication.java
@@ -0,0 +1,18 @@
+package demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableScheduling
+public class GpsSimulatorApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(GpsSimulatorApplication.class, args);
+    }
+
+}
---
+++ fleet-location-simulator/src/main/java/demo/service/PositionService.java
@@ -0,0 +1,13 @@
+package demo.service;
+
+
+import demo.model.CurrentPosition;
+
+public interface PositionService {
+
+    void processPositionInfo(long id,
+                             CurrentPosition currentPosition,
+                             boolean exportPositionsToKml,
+                             boolean sendPositionsToIngestionSerice);
+
+}
